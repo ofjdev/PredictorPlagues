bae2b41c1a7f16e42f6c3ea251094731d8d2ad54
ofjdev <ofj.dev@gmail.com>
2017-12-02 15:47:55 +0100

    Modified Background
    Esborrat el joc i dibuixades les plantacions

--- predictor_plagues.py
+++ predictor_plagues.py
@@ -6,16 +6,15 @@ import random
 
 blueP = (20, 34, 238)
 greenP = (20, 240, 50)
+green_chartreuse = (127, 255, 0)
 redP = (230, 0, 20)
 BLACK = (0, 0, 0)
 sizeSquare = 40
-px = int(raw_input("Coordenada en X: "))
-py = int(raw_input("Coordenada en Y: "))
 
 x = 0
 y = 0
 pygame.init()
-size = (600, 600)
+size = (800, 600)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Grid on PYGAME")
 clock = pygame.time.Clock()
@@ -26,29 +25,18 @@ while not gameOver:
         if event.type == pygame.QUIT:
             gameOver = True
     screen.fill(BLACK)
-    Fuente = pygame.font.Font(None, 16)
+    
     Tx = 0
     Ty = 0
     for i in range(1, size[0], 40):
         for j in range(1, size[1], 40):
-            pygame.draw.rect(screen, blueP, [i, j, 38, 38], 0)
-            if py == 0:
-                y = 1
-            elif py == Ty:
-                y = j
+            color_to_fill = greenP if (((j-1) // 40) % 2 == 1) else green_chartreuse
+            pygame.draw.rect(screen, color_to_fill, [i, j, 37, 37], 0)
             Ty += 1
-        if px == 0:
-            x = 1
-        elif px == Tx:
-            x = i
-        Texto = Fuente.render(str(Tx), True, greenP)
-        screen.blit(Texto, [i, 2])
-        if Tx != 0:
-            screen.blit(Texto, [2, i + 16])
         Tx += 1
         Ty = 0
     colAl = (random.randrange(1, 255), random.randrange(1, 255), random.randrange(1, 255))
-    pygame.draw.rect(screen, colAl, [x, y, 38, 38], 0)
+    #pygame.draw.rect(screen, colAl, [x, y, 38, 38], 0)
     pygame.display.flip()
-    clock.tick(5)
+    #clock.tick(5)
 pygame.quit()
